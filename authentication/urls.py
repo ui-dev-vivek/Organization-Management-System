62add5766f13863bf0097bb761304677b6c81421
amfarari <dev.vivek16@gmail.com>
2023-08-28 23:33:57 +0530

    Forgot Password/reset

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,5 +4,8 @@ from authentication.views import *
 urlpatterns = [
    path('' , user_login, name="auth.login" ),
    path('logout',user_logout,name="aurh.logout"),
-   path('subsidiaries/', subsidiaries,name='subsidiaries')
+   path('forgot-password',forgot_password,name='auth.forgot'),
+   path('reset-password/<uidb64>/<token>/',reset_password,name='auth.reset-pass'),
+   
+   # path('subsidiaries/', subsidiaries,name='subsidiaries')
 ]
