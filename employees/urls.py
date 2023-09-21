5e3fdde0474945690a69bb6217bb9d8967b1dd75
amfarari <dev.vivek16@gmail.com>
2023-09-21 09:44:28 +0530

    new

--- employees/urls.py
+++ employees/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('upload_profile_image/', upload_profile_image, name='upload_profile_image'),
     path('project/<str:slug>',project,name='emp.project'),
     path('project-task/',project_task_view,name='project_task'),
-    path('update-project-task/',update_project_task,name="update_project_task")
+    path('update-project-task/',update_project_task,name="update_project_task"),
+    path('project_task/<str:task_id>/delete/', delete_project_task, name='delete_project_task'),
+    path('project_task_checklist/<str:task_id>/delete/', delete_project_task_checklist, name='delete_project_task_checklist'),
+     path('upload_attachment/', upload_attachment, name='upload_attachment'),
 ]
